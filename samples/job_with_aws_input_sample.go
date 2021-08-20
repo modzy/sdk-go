5cd05acaa9e88827b8da9a2b29e008ccd3d2e3ca
Raul Casallas <raul.casallas@bairesdev.com>
2021-08-20 07:13:18 -0500

    feat: new samples

--- samples/job_with_aws_input_sample.go
+++ samples/job_with_aws_input_sample.go
@@ -35,18 +35,17 @@ func main() {
     // Get the model object:
     // If you already know the model identifier (i.e.: you got it from the URL of the model details page or from the input sample),
     // you can skip this step. If you don't, you can find the model identifier by using its name as follows:
-    //model, err := client.Models().GetModelDetailsByName(ctx, &modzy.GetModelDetailsByNameInput{Name: "Sentiment Analysis"})
+    model, err := client.Models().GetModelDetailsByName(ctx, &modzy.GetModelDetailsByNameInput{Name: "Facial Embedding"})
     // Or if you already know the model id and want to know more about the model, you can use this instead:
-    model, err := client.Models().GetModelDetails(ctx, &modzy.GetModelDetailsInput{ModelID: "ed542963de"})
+    // model, err := client.Models().GetModelDetails(ctx, &modzy.GetModelDetailsInput{ModelID: "f7e252e26a"})
     // You can find more information about how to query the models on the model_sample.go file.
     // The model identifier is under the ModelID key. You can take a look at the other properties under ModelDetails struct
-    // Or just log the model identifier, and potencially the latest version
+    // Or just log the model identifier, and potentially the latest version
     log.Printf("The model identifier is %s and the latest version is %s\n", model.Details.ModelID, model.Details.LatestVersion)
     // Get the model version object:
     // If you already know the model version and the input key(s) of the model version you can skip this step. Also, you can
     // use the following code block to know about the inputs keys and skip the call on future job submissions.
-    //modelVersion, err := client.Models().GetModelVersionDetails(ctx, &modzy.GetModelVersionDetailsInput{ModelID: model.Details.ModelID, Version: model.Details.LatestVersion})
-    modelVersion, err := client.Models().GetModelVersionDetails(ctx, &modzy.GetModelVersionDetailsInput{ModelID: model.Details.ModelID, Version: "0.0.27"})
+    modelVersion, err := client.Models().GetModelVersionDetails(ctx, &modzy.GetModelVersionDetailsInput{ModelID: model.Details.ModelID, Version: model.Details.LatestVersion})
     if err != nil {
         log.Fatalf("Unexpected error %s", err)
         return
@@ -54,7 +53,7 @@ func main() {
     // The info stored in modelVersion provides insights about the amount of time that the model can spend processing, the inputs, and
     // output keys of the model.
     log.Printf("The model version is %s\n", modelVersion.Details.Version)
-    log.Printf("  timeouts: status %dms, run %dms\n",modelVersion.Details.Timeout.Status, modelVersion.Details.Timeout.Run)
+    log.Printf("  timeouts: status %dms, run %dms\n", modelVersion.Details.Timeout.Status, modelVersion.Details.Timeout.Run)
     log.Println("  inputs:")
     for _, input := range modelVersion.Details.Inputs {
         log.Printf("    key %s, type %s, description: %s\n", input.Name, input.AcceptedMediaTypes, input.Description)
@@ -64,29 +63,48 @@ func main() {
         log.Printf("    key %s, type %s, description: %s\n", output.Name, output.MediaType, output.Description)
     }
     // Send the job:
-    // With the info about the model (identifier), the model version (version string, input/output keys), you are ready to
-    // submit the job. Just prepare the source map:
-    mapSource := make(map[string]modzy.TextInputItem)
-    mapInput := make(modzy.TextInputItem)
-    mapInput["input.txt"] = "Modzy is great!"
+    // Amazon Simple Storage Service (AWS S3) is an object storage service (for more info visit: https://aws.amazon.com/s3/?nc1=h_ls).
+    // It allows to store images, videos, or other content as files. In order to use as input type, provide the following properties:
+    //    AWS Access Key: replace <<AccessKey>>
+    AccessKey := "<<AccessKey>>"
+    //    AWS Secret Access Key: replace <<SecretAccessKey>>
+    SecretAccessKey := "<<SecretAccessKey>>"
+    //    AWS Default Region : replace <<AWSRegion>>
+    Region := "<<AWSRegion>>"
+    //    The Bucket Name: replace <<BucketName>>
+    BucketName := "<<BucketName>>"
+    //    The File Key: replace <<FileId>> (remember, this model needs an image as input)
+    FileKey := "<<FileId>>"
+    // With the info about the model (identifier) and the model version (version string, input/output keys), you are ready to
+    // submit the job. Just prepare the source dictionary:
+    mapSource := make(map[string]modzy.S3InputItem)
+    mapInput := make(modzy.S3InputItem)
+    mapInput["image"] = modzy.S3Input(BucketName, FileKey)
     mapSource["source-key"] = mapInput
-    // An inference job groups input data that you send to a model. You can send any amount of inputs to
-    // process and you can identify and refer to a specific input by the key that you assign, for example we can add:
-    mapInput = make(modzy.TextInputItem)
-    mapInput["input.txt"] = "Sometimes I really hate ribs"
+    // An inference job groups input data sent to a model. You can send any amount of inputs to
+    // process and you can identify and refer to a specific input by the key assigned. For example we can add:
+    mapInput = make(modzy.S3InputItem)
+    mapInput["image"] = modzy.S3Input(BucketName, FileKey)
     mapSource["second-key"] = mapInput
-    mapInput = make(modzy.TextInputItem)
-    mapInput["input.txt"] = "Born and raised in Pennsylvania, Swift moved to Nashville, Tennessee, at the age of 14 to pursue a career in country music"
+    mapInput = make(modzy.S3InputItem)
+    mapInput["image"] = modzy.S3Input(BucketName, FileKey)
     mapSource["another-key"] = mapInput
-    //If you send a wrong input key, the model fails to process the input.
-    mapInput = make(modzy.TextInputItem)
-    mapInput["a.wrong.key"] = "This input is wrong!"
+    // If you send a wrong input key, the model fails to process the input.
+    mapInput = make(modzy.S3InputItem)
+    mapInput["a.wrong.key"] = modzy.S3Input(BucketName, FileKey)
+    mapSource["wrong-key"] = mapInput
+    // If you send a correct input key, but a wrong AWS S3 value key, the model fails to process the input.
+    mapInput = make(modzy.S3InputItem)
+    mapInput["image"] = modzy.S3Input(BucketName, "wrong-aws-file-key.png")
     mapSource["wrong-key"] = mapInput
     // When you have all your inputs ready, you can use our helper method to submit the job as follows:
-    job, err := client.Jobs().SubmitJobText(ctx, &modzy.SubmitJobTextInput{
-        ModelIdentifier: model.Details.ModelID,
-        ModelVersion: modelVersion.Details.Version,
-        Inputs: mapSource,
+    job, err := client.Jobs().SubmitJobS3(ctx, &modzy.SubmitJobS3Input{
+        ModelIdentifier:    model.Details.ModelID,
+        ModelVersion:       modelVersion.Details.Version,
+        AWSAccessKeyID:     AccessKey,
+        AWSSecretAccessKey: SecretAccessKey,
+        AWSRegion:          Region,
+        Inputs:             mapSource,
     })
     if err != nil {
         log.Fatalf("Unexpected error %s", err)
@@ -99,7 +117,7 @@ func main() {
     // We provide a helper method to listen until the job finishes processing. Its a good practice to set a max timeout
     // if you're doing a test (ie: 2*status+run). Otherwise, if the timeout is set to None, it will listen until the job finishes and moves to
     // COMPLETED, CANCELED, or TIMEOUT.
-    job2, err := job.JobActions.WaitForCompletion(ctx, 20*time.Second)
+    job2, err := client.Jobs().WaitForJobCompletion(ctx, &modzy.WaitForJobCompletionInput{JobIdentifier: job.Response.JobIdentifier}, 20*time.Second)
     if err != nil {
         log.Fatalf("Unexpected error %s", err)
         return
@@ -109,9 +127,7 @@ func main() {
     if job2.Details.Status == "COMPLETED" {
         // A completed job means that all the inputs were processed by the model. Check the results for each
         // input key provided in the source map to see the model output.
-        results, err := job.JobActions.GetResults(ctx)
-        // You can also get the results with the identifier (if you don't have the job object)
-        //results, err := client.Jobs().GetJobResults(ctx, &modzy.GetJobResultsInput{JobIdentifier: job.Response.JobIdentifier})
+        results, err := client.Jobs().GetJobResults(ctx, &modzy.GetJobResultsInput{JobIdentifier: job.Response.JobIdentifier})
         if err != nil {
             log.Fatalf("Unexpected error %s", err)
             return
