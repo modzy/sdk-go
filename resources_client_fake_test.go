b1515348a612c7cf3cf6ec3ff3114e79082ceed7
Matt Holcomb <yorktownmatt@gmail.com>
2021-08-30 22:27:48 -0400

    adding two missing client fakes (#31)

---
+++ resources_client_fake_test.go
@@ -0,0 +1,27 @@
+package modzy
+
+import (
+	"context"
+	"testing"
+)
+
+func TestResourcesClientFake(t *testing.T) {
+	expectedCtx := context.WithValue(context.TODO(), testContextKey("a"), "b")
+
+	calls := 0
+	fake := &ResourcesClientFake{
+		GetProcessingModelsFunc: func(ctx context.Context) (*GetProcessingModelsOutput, error) {
+			calls++
+			if ctx != expectedCtx {
+				t.Errorf("not expected ctx")
+			}
+			return nil, nil
+		},
+	}
+
+	fake.GetProcessingModels(expectedCtx)
+
+	if calls != 1 {
+		t.Errorf("Did not call all of the funcs: %d", calls)
+	}
+}
